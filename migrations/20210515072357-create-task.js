369009aa6fc7b5dccf4fd292815b0a3f94420253
Mikhael6363 <mikhael.6363@gmail.com>
2021-05-25 00:40:45 +0300

    Add task migration

---
+++ migrations/20210515072357-create-task.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      deadline: {
+        type: Sequelize.DATE,
+      },
+      isDone: {
+        field: 'is_done',
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('tasks');
+  },
+};
